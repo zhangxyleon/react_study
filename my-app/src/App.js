eff9dba79a0236fcac8c0b26f28a9da4df453860
Xiangyu Zhang <leonfigocr17@gmail.com>
2020-07-14 15:00:22 -0400

    part1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import {
+  Navbar,
+  NavbarBrand,
+} from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
